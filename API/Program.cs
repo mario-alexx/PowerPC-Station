dcfaf8bd6fdb0452e8b849442b01510b44451d94
mario-alexx <marioatoroh@gmail.com>
2024-08-28 22:55:32 -0300

    adding connection string and register services

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,17 @@
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt => {
+  opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
