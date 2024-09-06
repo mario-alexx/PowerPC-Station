21057a002f19349105a3bf697258a5ded9ebaaef
mario-alexx <marioatoroh@gmail.com>
2024-09-06 00:30:33 -0300

    adding validation to paging enabled in specification evaluator

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -30,6 +30,9 @@ public class SpecificationEvaluator<T> where T : BaseEntity
     if(spec.IsDistinct)
       query = query.Distinct();
     
+    if(spec.IsPagingEnabled)
+      query = query.Skip(spec.Skip).Take(spec.Take);
+
     return query;
   }
 
@@ -54,11 +57,14 @@ public class SpecificationEvaluator<T> where T : BaseEntity
     
     var selectQuery = query as IQueryable<TResult>;
 
-    if(spec != null) 
+    if(spec.Select != null) 
       selectQuery = query.Select(spec.Select);
     
     if(spec.IsDistinct) 
       selectQuery = selectQuery?.Distinct();
+    
+    if(spec.IsPagingEnabled)
+      selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
     
     return selectQuery ?? query.Cast<TResult>();;
   }
