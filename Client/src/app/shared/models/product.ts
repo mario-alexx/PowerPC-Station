4bd5c5d627a8c882c1569a59615eaa93f02ed87b
mario-alexx <marioatoroh@gmail.com>
2024-09-10 00:26:34 -0300

    adding pagination and product interfaces

---
+++ Client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  quantityInStock: number;
+  brand: string;
+  type: string;
+  pictureUrl: string;
+}
