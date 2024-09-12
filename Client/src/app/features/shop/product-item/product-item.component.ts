8aa433aab1f79f5e093585c7c034226a09ab722f
mario-alexx <marioatoroh@gmail.com>
2024-09-12 00:58:56 -0300

    adding product-item component

---
+++ Client/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    MatIcon,
+    CurrencyPipe
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss'
+})
+/**
+ * ProductItemComponent displays individual product details.
+ */
+export class ProductItemComponent {
+  /**
+   * The product data to be displayed in the component.
+   * 
+   * @type {Product}
+   * @memberof ProductItemComponent
+  */
+  @Input() product?: Product;
+}
