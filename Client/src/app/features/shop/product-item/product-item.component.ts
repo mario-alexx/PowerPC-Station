5681c9a1a4d9886c0fd35eb5fc3d7cb2d4f1a545
mario-alexx <marioatoroh@gmail.com>
2024-09-15 01:31:32 -0300

    inject cart service in product-item component

--- Client/src/app/features/shop/product-item/product-item.component.ts
+++ Client/src/app/features/shop/product-item/product-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../../core/services/cart.service';
 
+/**
+ * ProductItemComponent displays individual product details.
+ */
 @Component({
   selector: 'app-product-item',
   standalone: true,
@@ -21,15 +25,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
-/**
- * ProductItemComponent displays individual product details.
- */
 export class ProductItemComponent {
   /**
    * The product data to be displayed in the component.
-   * 
    * @type {Product}
    * @memberof ProductItemComponent
   */
   @Input() product?: Product;
+  
+  /** Injects the CartService to handle cart-related actions. */
+  cartService = inject(CartService);
+
 }
