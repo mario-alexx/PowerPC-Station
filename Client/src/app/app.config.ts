4f101c391b5f75e4422a3e227acd70a1812a891b
mario-alexx <marioatoroh@gmail.com>
2024-09-15 01:36:50 -0300

    add app initializer

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,12 +6,36 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorsInterceptor } from './core/interceptors/errors.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
+
+/**
+ * Initializes the application by invoking the InitService to load initial data.
+ * Removes the splash screen once the initialization is complete.
+ *
+ * @param {InitService} initService - The service responsible for loading initial data.
+ * @returns {() => Promise<void>} A function that returns a Promise which resolves after the initialization process.
+ */
+function initializeApp(initService: InitService) 
+{
+  return () => lastValueFrom(initService.init()).finally( () => {
+    const splash = document.getElementById('initial-splash');
+    if(splash) 
+      splash.remove();
+  })
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([errorsInterceptor, loadingInterceptor]))
+    provideHttpClient(withInterceptors([errorsInterceptor, loadingInterceptor])),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+      deps: [InitService]
+    }
   ]
 };
