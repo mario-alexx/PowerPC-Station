1f8e0bc957f4d41f041b6e0c9efbf8daacf55d02
mario-alexx <marioatoroh@gmail.com>
2024-09-17 12:16:57 -0300

    adding address DbSet entity and inherits to IdentityDbContext

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,17 +1,19 @@
 using Core.Entities;
 using Infrastructure.Config;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
 /// <summary>
-/// Represents the database context for the store, providing access to entities and configuration settings.
-/// Initializes a new instance of the <see cref="StoreContext"/> class using the specified options.
+/// Represents the database context for the store application, 
+/// including identity functionality for users.
 /// </summary>
-/// <param name="options">The options to be used by a <see cref="DbContext"/>.</param>
-public class StoreContext(DbContextOptions options) : DbContext(options)
+/// <param name="options">Options to configure the database context <see cref="IdentityDbContext"/>.</param>
+public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
   public DbSet<Product> Products{ get; set; }
+  public DbSet<Address> Addresses{ get; set; }
 
   /// <summary>
   /// Configures the model that was discovered by convention from the entity types exposed in <see cref="DbSet{TEntity}"/> properties on the derived context.
