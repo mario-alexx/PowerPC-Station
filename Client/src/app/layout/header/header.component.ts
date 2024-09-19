b735e3097955faf03bf7eae27a693d4b426dc1c3
mario-alexx <marioatoroh@gmail.com>
2024-09-19 12:05:44 -0300

    updating header component: add mat-menu, account service, logout

--- Client/src/app/layout/header/header.component.ts
+++ Client/src/app/layout/header/header.component.ts
@@ -2,10 +2,13 @@ import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
+import { AccountService } from '../../core/services/account.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 /**
  * Component responsible for displaying the application header,
@@ -20,7 +23,11 @@ import { CartService } from '../../core/services/cart.service';
     MatBadge,
     RouterLink,
     RouterLinkActive,
-    MatProgressBar
+    MatProgressBar,
+    MatMenuTrigger,
+    MatMenu,
+    MatDivider,
+    MatMenuItem
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -31,4 +38,27 @@ export class HeaderComponent {
 
   /** Injects the CartService to access cart-related operations. */
   cartService = inject(CartService);
+
+  /**
+   * Injected service to handle account-related operations.
+  */
+  accountService = inject(AccountService); 
+
+  /**
+   * Injected router to navigate between different routes.
+  */
+  router = inject(Router);
+
+  /**
+   * Logs out the current user and redirects to the home page.
+   */
+  logout(): void
+  {
+    this.accountService.logout().subscribe({
+      next: () => {
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+      }
+    });
+  }
 }
