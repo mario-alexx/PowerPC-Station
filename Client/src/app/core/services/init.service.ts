3cbfa27901a3f9db94ce9ab665d09b3f462d5d52
mario-alexx <marioatoroh@gmail.com>
2024-09-19 11:37:08 -0300

    adding accountService in initService

--- Client/src/app/core/services/init.service.ts
+++ Client/src/app/core/services/init.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { Cart } from '../../shared/models/cart';
+import { AccountService } from './account.service';
 
 /**
- * Service responsible for initializing the application state, specifically for handling the shopping cart.
+ * InitService handles the initialization process of the application by fetching the user and cart data.
 */
 @Injectable({
   providedIn: 'root'
@@ -14,14 +15,22 @@ export class InitService {
   /** Injects the CartService to interact with cart operations */
   private cartService = inject(CartService);
 
+  /** Injects the AccountService to manage user-related operations. */
+  private accountService = inject(AccountService);
+
   /**
-   * Initializes the application by retrieving the cart if it exists in localStorage.
-   * @returns An observable of the Cart if a cart ID is found, or null if no cart exists.
+   * Initializes the application by fetching the cart and user information.
+   * It checks for the existence of a `cart_id` in the local storage and retrieves the associated cart if found.
+   * It also retrieves the current user's information if the user is authenticated.
+   * @returns An observable that combines both the cart and user information using forkJoin.
   */
-  init(): Observable<Cart> | Observable<null>
+  init(): Observable<{}>
   {
     const cartId = localStorage.getItem('cart_id');
     const cart$ = cartId ? this.cartService.getCart(cartId) : of(null);
-    return cart$;
+    return forkJoin({
+      cart: cart$,
+      user: this.accountService.getUserInfo()
+    })
   }
 }
