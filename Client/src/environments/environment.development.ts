dec66b401f5de34db27582e3e22967d082932dcc
mario-alexx <marioatoroh@gmail.com>
2024-09-23 11:06:22 -0300

    adding stripe public key

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = 
 {
   production: false,
-  apiUrl: 'https://localhost:5001/api/'
+  apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey: 'pk_test_51Q0pDKJC9jcW1tDuH4Cx9XsmNA5TODCZ0tSwNcmNVbxxfEDRkdL8nrYIQDUVPN0z7UwxwEdqx7h4fypV84S2r8nP00r5F14xrH'
 };
--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = 
 {
   production: true,
-  apiUrl: 'api/'
+  apiUrl: 'api/',
+  stripePublicKey: 'pk_test_51Q0pDKJC9jcW1tDuH4Cx9XsmNA5TODCZ0tSwNcmNVbxxfEDRkdL8nrYIQDUVPN0z7UwxwEdqx7h4fypV84S2r8nP00r5F14xrH'
 };
