dea5ce0c3666836235358ae9388fa83f16fdc061
mario-alexx <marioatoroh@gmail.com>
2024-09-25 11:02:17 -0300

    adding orderStatus(enum) entity

---
+++ Core/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,11 @@
+namespace Core.Entities.OrderAggregate;
+
+/// <summary>
+/// Represents the status of an order.
+/// </summary>
+public enum OrderStatus
+{
+  Pending,
+  PaymentReceived,
+  PaymentFailed
+}
