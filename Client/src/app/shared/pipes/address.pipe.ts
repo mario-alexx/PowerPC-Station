0a93479e2263ab8789d1f062ae9a6c807a414a5d
mario-alexx <marioatoroh@gmail.com>
2024-09-27 12:37:09 -0300

    updating address Pipe

--- Client/src/app/shared/pipes/address.pipe.ts
+++ Client/src/app/shared/pipes/address.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ConfirmationToken } from '@stripe/stripe-js';
+import { ShippingAddress } from '../models/order';
 
 /**
  * Pipe to transform a shipping address into a formatted string for display.
@@ -11,22 +12,27 @@ import { ConfirmationToken } from '@stripe/stripe-js';
 export class AddressPipe implements PipeTransform {
   
   /**
-   * Transforms the shipping object into a formatted address string.
-   * 
-   * @param value - The shipping object containing the address and name.
-   * @returns A formatted string of the shipping address or 'Unknown address' if data is missing.
-  */
-  transform(value?: ConfirmationToken['shipping'], ...args: unknown[]): unknown {
-    if(value?.address && value.name)
+   * Transforms the provided address data into a string format for display.
+   * @param value - The shipping address data to be transformed.
+   * @param args - Additional arguments (not used in this case).
+   * @returns The transformed address string or `null` if no valid data is provided.
+   */
+  transform(value?: ConfirmationToken['shipping'] | ShippingAddress, ...args: unknown[]): unknown {
+    if(value && 'address' in value && value.name)
     {
-      const {line1, line2, city, state, country, postal_code} = value.address;
+      const {line1, line2, city, state, country, postal_code} = (value as ConfirmationToken['shipping'])?.address!;
       return `${value.name}, ${line1}${line2 ? ', ' + line2 : ''}, ${city},
        ${state}, ${postal_code}, ${country}`;
+    }
+    else if(value && 'line1' in value)
+    {
+      const {line1, line2, city, state, country, postalCode} = value as ShippingAddress
+      return `${value.name}, ${line1}${line2 ? ', ' + line2 : ''}, ${city},
+       ${state}, ${postalCode}, ${country}`;
     }
     else 
     {
       return 'Unknown address';
     }
   }
-
 }
