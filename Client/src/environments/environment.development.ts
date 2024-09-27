0cf4a0535f2c8a57ab59bb889d8bdd182bc74dad
mario-alexx <marioatoroh@gmail.com>
2024-09-27 12:40:56 -0300

    adding hubUrl

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment =
 {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
   stripePublicKey: 'pk_test_51Q0pDKJC9jcW1tDuH4Cx9XsmNA5TODCZ0tSwNcmNVbxxfEDRkdL8nrYIQDUVPN0z7UwxwEdqx7h4fypV84S2r8nP00r5F14xrH'
 };
