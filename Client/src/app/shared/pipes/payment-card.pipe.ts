bbbdd55fec87db912364cac29a23ea5d2c8a636d
mario-alexx <marioatoroh@gmail.com>
2024-09-27 12:37:24 -0300

    updating payment-card Pipe

--- Client/src/app/shared/pipes/payment-card.pipe.ts
+++ Client/src/app/shared/pipes/payment-card.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ConfirmationToken } from '@stripe/stripe-js';
+import { PaymentSummary } from '../models/order';
 
 /**
  * Pipe to transform payment card information into a formatted string for display.
@@ -11,16 +12,21 @@ import { ConfirmationToken } from '@stripe/stripe-js';
 export class PaymentCardPipe implements PipeTransform {
 
   /**
-   * Transforms the payment method object into a formatted payment card string.
-   * 
-   * @param value - The payment method preview object containing card details.
-   * @returns A formatted string of the card brand, last 4 digits, and expiration date or 'Unknown payment method' if data is missing.
-  */
-  transform(value?: ConfirmationToken['payment_method_preview'], ...args: unknown[]): unknown {
-    if(value?.card) 
+   * Transforms the provided payment card data into a string format for display.
+   * @param value - The payment method preview or payment summary to be transformed.
+   * @param args - Additional arguments (not used in this case).
+   * @returns The transformed payment card information or `null` if no valid data is provided.
+   */
+  transform(value?: ConfirmationToken['payment_method_preview'] | PaymentSummary, ...args: unknown[]): unknown {
+    if(value && 'card' in value) 
     {
-      const {brand, last4, exp_month, exp_year} = value.card;
+      const {brand, last4, exp_month, exp_year} = (value as ConfirmationToken['payment_method_preview']).card!;
       return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${exp_month}/${exp_year}`;
+    }
+    else if(value && 'last4' in value)
+    {
+      const {brand, last4, expMonth, expYear} = value as PaymentSummary;
+      return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${expMonth}/${expYear}`
     }
     else 
     {
